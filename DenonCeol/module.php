0a0fdb1128ce4d57cc0632fad079903294663b04
SymPiTo <39837395+SymPiTo@users.noreply.github.com>
2018-06-01 19:52:54 +0200

    Update module.php

--- DenonCeol/module.php
+++ DenonCeol/module.php
@@ -1,6 +1,6 @@
 <?
     // Klassendefinition
-    class ModulnameXYZ extends IPSModule {
+    class DenonCeol extends IPSModule {
  
         // Der Konstruktor des Moduls
         // Überschreibt den Standard Kontruktor von IPS
